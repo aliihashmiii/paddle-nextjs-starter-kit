5153535689d6b546c69bb832870d0e9c5b5ed5a1
aliihashmiii <alihashmi.1@outlook.com>
2026-02-15 03:40:01 +0300

    Refactor RootLayout and clean up imports
    Removed unused imports and updated metadata structure.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,9 @@
-import { Inter } from 'next/font/google';
-import '../styles/globals.css';
-import '../styles/layout.css';
-import { ReactNode } from 'react';
-import type { Metadata } from 'next';
-import { Toaster } from '@/components/ui/toaster';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  metadataBase: new URL('https://paddle-billing.vercel.app'),
-  title: 'AeroEdit',
-  description:
-    'AeroEdit is a powerful team design collaboration app and image editor. With plans for businesses of all sizes, streamline your workflow with real-time collaboration, advanced editing tools, and seamless project management.',
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={'min-h-full dark'}>
-      <body className={inter.className}>
-        {children}
-        <Toaster />
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
